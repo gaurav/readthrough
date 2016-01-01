10ab26cf3206a70168ae303ca48b6aeb0f3136f9
gaurav <gaurav@ggvaidya.com>
2016-01-01 03:29:55 -0700

    Basic user authentication via Pocket.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // Load some libraries.
 var http = require('http');
 var https = require('https');
+var os = require('os');
+
+// Set up .env so we have the same environment as heroku.
+require('dotenv').load();
 
 // Set up Express middleware.
 var express = require('express');
@@ -13,6 +17,10 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+// Set up Passport for authentication.
+const passport = require('passport');
+const PocketStrategy = require('passport-pocket');
 
 // Set up database connection with MongoDB.
 const mongoose = require('mongoose');
@@ -28,24 +36,67 @@ app.use(session({
     saveUninitialized: false, // maybe?
     store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set up Pocket-based authentication.
+if(!process.env.POCKET_CONSUMER_KEY) {
+    throw new Error("No POCKET_CONSUMER_KEY specified!");
+}
+
+// Passport Set serializers
+passport.serializeUser(function(user, done) {
+    user['displayName'] = user['username'];
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+var pocketStrategy = new PocketStrategy({
+    consumerKey:    process.env.POCKET_CONSUMER_KEY,
+    callbackURL:    "http://" + os.hostname() + ":" + app.get('port') + "/login/pocket/callback",
+}, function(username, accessToken, done) {
+    process.nextTick(function() {
+        return done(null, {
+            username: username,
+            accessToken: accessToken
+        });
+    });
+});
+
+passport.use(pocketStrategy);
+
+// Authentication routes.
+app.get('/login/pocket', passport.authenticate('pocket'), function(req, res) {
+    res.redirect('/');
+});
+app.get('/login/pocket/callback', passport.authenticate('pocket', { failureRedirect: '/login' }),
+function(req, res) {
+    res.redirect('/');
+});
+
+// And when it all goes wrong.
+app.get('/logout', function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
+});
 
 // The login page is most people's first introduction
 // to the app.
 app.get('/login', function(req, res) {
     res.render('pages/login', {
         page_title: 'Log in or create an account',
-        user: null
+        user: null,
+        error: req.query.error
     });
 });
 
-// Login to Pocket using OAuth.
-
-// Set up PassportJS for authentication.
-
 // This is as far as you get without a login.
 function get_user_or_redirect(req, res) {
-    if('user' in req.session) {
-        return req.session.user;
+    if('user' in req) {
+        return req.user;
     }
 
     res.redirect('/login');
@@ -63,7 +114,7 @@ app.get('/', function(req, res) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
